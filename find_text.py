4bab29427ea9fcc4e1950a245704a20d20117262
Alex Bennee <ajb@cbnl.com>
2011-01-05 14:20:31 +0000

    Initial version

---
+++ find_text.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# find_text - a simple text finder written in Python
+#
+# This program is designed to extract text files from binary dumps. It was written
+# to extract text data from file-system dumps. I wrote it because tools like scalpel
+# and foremost while great for extracting structured data with headers and footers kinda
+# sucks for plain text.
+#
+# The sequence is simple:
+#   1. start at block 0
+#   2. are the bytes ASCII text
+#     - no skip to next block, start again
+#     - yes continue until
+#       - start getting zeros (end of block)
+#       - start getting gibberish (throw away goto next block) 
+#
+# (C) Alex Bennée
+# Available under GPLv3
+#
+
+import os
+import getopt,sys
+import re
+
+# A good basic block size which matches most underlying disk hardware
+# This can be specified to larger number to align with file-system blocks
+blocksize=512
+
+# Shall we be verbose?
+verbose=False
+
+# Regex objects
+all_text=re.compile("[\s\x20-\x7e]{512}")
+terminated_text=re.compile("[\s\x20-\x7e]{1,511}\x00")
+
+# Magic text, if set the existence of this in recovered text will cause it to dump
+magic_text=None
+
+# Heuristic, minimum number of words for 'valid' text
+min_words=20
+
+def save_recovered_text(text):
+    """
+    Save text out to a file on the disk
+    """
+    if verbose: print "save_recovered_text: saving:\n%s\n" % (text)
+    return
+
+def handle_recovered_text(text):
+    """
+    Do some heuristics on the recovered text and decide
+    if we want to dump it out.
+    """
+    #if verbose: print "handle_recovered_text: checking:\n%s\n" % (text)
+    
+    if magic_text:
+        if text.find(magic_text)>=0:
+            save_recovered_text(text)
+    else:
+        words = text.split()
+        if len(words)>min_words:
+            save_recovered_text(text)
+    
+
+def process_dump_block(f, pos):
+    #if verbose: print "process_dump_block, position = %d" % (pos)
+    
+    f.seek(pos)
+    bytes = 0
+    done = False
+    recovered = ""
+    
+    while not done:
+        data = f.read(blocksize)
+        bytes = bytes + blocksize
+
+        m = all_text.match(data)
+        if m:
+            recovered += data
+        else:
+            m = terminated_text.match(data)
+            if m:
+                recovered += data[m.start():m.end()-1]
+            done = True
+
+    # if we recovered any text we should print it
+    if len(recovered)>0:
+        handle_recovered_text(recovered)
+
+    #if verbose: print "process_dump_block done after %d bytes" % (bytes)
+    return bytes
+
+def process_dump_file(filename):
+    if verbose: print "process_dump_file: checking %s" % (filename)
+
+    stat = os.stat(filename)
+    done = False
+    p = 0
+    f = open(filename, "r")
+
+    while not done:
+        bytes = process_dump_block(f, p)
+        p = p + bytes
+        if p > stat.st_size:
+            done = True
+
+    if verbose: print "process_dump_file: done with %s" % (filename)
+
+def usage():
+    print "%s [-v] [FILE1 [FILE2 [..]]]" % (sys.argv[0])
+    print """
+  -h, --help            : this help message
+  -v, --verbose         : verbose output
+  -m, --magic="string"  : treat a recovered hunk as valid if it contains this string
+"""
+
+    sys.exit(1)
+
+if __name__ == "__main__":
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "hvm:", ["help", "verbose", "magic="])
+    except getopt.GetoptError, err:
+        usage()
+
+    for o,a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        if o in ("-v", "--verbose"):
+            verbose=True
+        if o in ("-m", "--magic"):
+            magic_text=a
+
+    for arg in args:
+        process_dump_file(arg)
